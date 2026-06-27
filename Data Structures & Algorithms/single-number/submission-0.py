aff438e720b26b22c271d2224a1b09f99eb2d231
Kevin Shin <kevnshin@gmail.com>
2026-06-26 22:30:55 -1000

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mask = 0
+
+        for num in nums:
+            bit = 1 << (num + 10000)
+            if mask & bit:
+                mask &= ~bit
+            else:
+                mask |= bit
+
+
+        return mask.bit_length() - 1 - 10000
