aba49ad518a91015025cdc1b835007181c257cc6
Kevin Shin <kevnshin@gmail.com>
2026-07-29 01:07:10 -1000

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        foundCycle = False
+        prevNums = set()
+        number = n
+
+        while not foundCycle:
+            thousands = (number // 1000) % 10
+            hundreds = (number // 100) % 10
+            tens = (number // 10) % 10
+            ones = number % 10
+
+            sumOfSquares = thousands ** 2 + hundreds ** 2 + tens ** 2 + ones ** 2
+            if sumOfSquares == 1:
+                return True
+            elif sumOfSquares in prevNums:
+                return False
+            else:
+                prevNums.add(sumOfSquares)
+            number = sumOfSquares
+            
+
+
+        
