6eb5df3e2cf72f34f099d226a0b753a54ff93708
Kevin Shin <kevnshin@gmail.com>
2026-08-12 21:52:34 -1000

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        positionSpeeds = {}
+        for i, p in enumerate(position):
+            positionSpeeds[p] = speed[i]
+
+        sortedPositions = sorted(position, reverse=True)
+
+        fleets = 0
+        times = []
+        referenceTime = None
+        for i, position in enumerate(sortedPositions):
+            time = (target - position) / positionSpeeds[position]
+            times.append(time)
+
+            if referenceTime == None:
+                referenceTime = time
+
+            if i - 1 < 0 or referenceTime < time:
+                fleets += 1
+
+            referenceTime = max(referenceTime, time)
+        return fleets
+
