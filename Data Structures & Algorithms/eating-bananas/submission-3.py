1fed2396df35ea8f3c30043bda4fa26ec5598f94
Kevin Shin <kevnshin@gmail.com>
2026-08-14 11:49:00 -1000

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        upperBound = max(piles)
+        kRange = range(1, upperBound + 1)
+        left = 0
+        right = upperBound
+        while left < right:
+            time = 0
+            kIndex = (left + right) // 2
+            for pile in piles:
+                time += math.ceil(pile / kRange[kIndex])
+            if time <= h:
+                result = kRange[kIndex]
+                right = kIndex
+            else:
+                left = kIndex + 1
+        return result
+            
