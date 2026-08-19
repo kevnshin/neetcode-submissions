26909ec0d10001ab777093f2df179782f54bd0f4
Kevin Shin <kevnshin@gmail.com>
2026-08-18 22:32:43 -1000

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+import copy
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        # s1Map = defaultdict(int)
+        # for s in s1:
+        #     s1Map[s] += 1
+        # foundLetterInS1 = False
+        # # print('s1Map', s1Map)
+        s1Frequency = self.tranformToListStr(s1)
+        left, right = 0, len(s1)
+        # s1MapCopy = None
+        while right <= len(s2):
+            if s1Frequency == self.tranformToListStr(s2[left:right]):
+                return True
+            left += 1
+            right += 1
+        return False
+
+        #     if s2[right] in s1Map:
+        #         # print('right', right)
+        #         # print('s2[right]', s2[right])
+        #         # print('left', left)
+        #         # print('s2[left]', s2[left])
+        #         if not foundLetterInS1:
+        #             foundLetterInS1 = True
+        #             s1MapCopy = copy.deepcopy(s1Map)
+        #         else:
+        #             if s1MapCopy[s2[right]] == 0:
+        #                 s1MapCopy[s2[left]] += 1
+        #                 left += 1
+        #                 continue
+                    
+        #         s1MapCopy[s2[right]] -= 1
+        #     else:
+        #         foundLetterInS1 = False
+        #     # if s1MapCopy:
+        #         # print('s1MapCopy', s1MapCopy)
+            
+        #     if foundLetterInS1 and all(value == 0 for value in s1MapCopy.values()):
+        #         return True
+
+        #     right += 1
+        #     if not foundLetterInS1:
+        #         left +=1
+
+        # # for s in s2:
+        # #     if s in s1Map:
+        # #         if not foundLetterInS1:
+        # #             foundLetterInS1 = True
+        # #             s1MapCopy = copy.deepcopy(s1Map)
+
+        # #         s1MapCopy[s] -= 1
+        # #         if s1MapCopy[s] == 0:
+        # #             del s1MapCopy[s]
+        # #     else:
+        # #         foundLetterInS1 = False
+        # #     if foundLetterInS1 and len(s1MapCopy) == 0:
+        # #         return True
+        # return False
+
+    def tranformToListStr(self, word:str) -> Tuple[int]:
+        result = [0] * 26
+        for char in word:
+            result[self.transformToIndex(char)] += 1
+        return tuple(result)
+
+
+    def transformToIndex(self, s:str) -> int:
+        return ord(s) - ord('a')
